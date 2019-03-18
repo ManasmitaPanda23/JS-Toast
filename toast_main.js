c616063ece5072cdda1a20ae346589790f873bfe
ManasmitaPanda23 <48487835+ManasmitaPanda23@users.noreply.github.com>
2019-03-18 15:19:48 +0530

    Add files via upload

---
+++ toast_main.js
@@ -0,0 +1,10 @@
+function showToast() {
+    document.getElementById("toast").style.display = "block";
+
+
+    setTimeout(function () {
+        document.getElementById("toast").style.display = "none";
+    }, 2000);
+
+
+}
